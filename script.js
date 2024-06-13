58d138aa493b2ee121f0730098d4a792603d7209
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-06-14 00:29:40 +0100

    added banner, navbar

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//select the nav trigger btn
+const navTrigger = document.querySelector("#navTrigger");
 
+//select navigation
+const nav = document.querySelector("#nav");
+
+//select th nav close btn
+const navCloseBtn = document.querySelector("#navClosebtn");
+
+//add a click event listener to the nav trigger btn
+navTrigger.addEventListener("click", () => {
+  //toggle the "is-open" class on the navigation element to open/close the menu
+  nav.classList.toggle("is-open");
+});
+
+//add a click event listener to the nav close btn
+navCloseBtn.addEventListener("click", () => {
+  //remove the "is-open" class from the navigation element
+  nav.classList.remove("is-open");
+});
